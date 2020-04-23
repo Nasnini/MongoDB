bc3abbfb5b2f40e87baa36fe18f89c72e8ffdeb9
Nasnini <theo.kobuoe@umuzi.com>
2020-04-23 23:20:06 +0200

    Added functions

--- PyUmuziProspects.py
+++ PyUmuziProspects.py
@@ -5,3 +5,29 @@ client = MongoClient("mongodb://root:rootpassword@localhost:27017")
 
 db = client["pymongo_UmuziProspects"]
 my_db = db["Visiter"]
+
+db.Visiter.insert({id: "", visitors_name: "Nasni", visitors_age: "25", date_of_visit: "2020-03-26", time_of_visit: "12:30", assisted_by: "Nthombi Brown", comments: "student at Umuzi"})
+
+# create_visitor
+def create_visitor():
+    pass
+
+# list_visitors
+def list_visitors(self, parameter_list):
+    pass
+
+# delete_visitor
+def delete_visitor():
+    pass
+
+# update_visitor
+def update_visitor():
+    pass
+
+# visitor_details
+def visitor_details():
+    pass
+
+# delete_all
+def delete_all():
+    pass
