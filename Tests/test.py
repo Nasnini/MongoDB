25c76a054bf02e3dedcf1304a4fa299140f4fd64
nasnini <theo.kobuoe@umuzi.org>
2020-05-15 10:43:59 +0200

    Tests added

--- Tests/test.py
+++ Tests/test.py
@@ -1,7 +1,29 @@
+from PyUmuziProspects import *
+import unittest
+from mongo_engine_code import *
 
+class test_mongo(unittest.TestCase):
+    
+    # Test if the Visitor is saved into the collection
+    def test_Create_Visitor(self):
+        assert create_visitor
 
-db.Visiter.insert({id: "", visitors_name: "Nasni", visitors_age: "25", date_of_visit: "2020-03-26", time_of_visit: "12:30", assisted_by: "Nthombi Brown", comments: "student at Umuzi"})
+    # Test should return an array of all the visitor names and ids
+    def test_List_Visitor(self):
+        assert list_visitor
 
+    # Test if the visitor is successfully deleted
+    def test_Delete_Visitor(self):
+        assert delete_visitor
 
-#db.Visiter.find()
-#{ "_id" : ObjectId("5ea188085fba28d55e3a4e27"), "id" : "", "visitors_name" : "Nasni", "visitors_age" : "25", "date_of_visit" : "2020-03-26", "time_of_visit" : "12:30", "assisted_by" : "Nthombi Brown", "comments" : "student at Umuzi" }
+    # Test if only a single documnet is updated in the collection
+    def test_Update_Visitor(self):
+        assert update_visitor
+
+    # Test if ID returns information about the visitor
+    def test_Visitor_Details(self):
+        assert visitor_details
+
+    # Test if all the collections are deleted
+    def test_Delete_All(self):
+        assert delete_all
